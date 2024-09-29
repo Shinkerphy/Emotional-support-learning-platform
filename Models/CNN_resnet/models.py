a4bfd8949734fd79e268d6df9a420cd5885af2c5
Abdulmalik <abdulmalikshinkafi@gmail.com>
2024-09-29 12:03:12 +0100

    Added ResNet Baseline

---
+++ Models/CNN_resnet/models.py
@@ -0,0 +1,48 @@
+import torch.nn as nn
+import torch.nn.functional as F
+from torchvision import models
+
+class EmotionCNN(nn.Module):
+    def __init__(self):
+        super(EmotionCNN, self).__init__()
+        self.conv1 = nn.Conv2d(1, 32, kernel_size=3, padding=1)
+        self.conv2 = nn.Conv2d(32, 64, kernel_size=3, padding=1)
+        self.conv3 = nn.Conv2d(64, 128, kernel_size=3, padding=1)
+        self.conv4 = nn.Conv2d(128, 256, kernel_size=3, padding=1)
+        self.conv5 = nn.Conv2d(256, 512, kernel_size=3, padding=1)
+        self.pool = nn.MaxPool2d(2, 2)
+        self.dropout = nn.Dropout(0.5)
+        self.bn1 = nn.BatchNorm2d(32)
+        self.bn2 = nn.BatchNorm2d(64)
+        self.bn3 = nn.BatchNorm2d(128)
+        self.bn4 = nn.BatchNorm2d(256)
+        self.bn5 = nn.BatchNorm2d(512)
+        self.fc1 = nn.Linear(512 * 7 * 7, 1024)  # Adjusted for 224x224 input
+        self.fc2 = nn.Linear(1024, 256)
+        self.fc3 = nn.Linear(256, 7)
+        self.relu = nn.ReLU()
+        self.leaky_relu = nn.LeakyReLU(0.1)
+    
+    def forward(self, x):
+        x = self.pool(self.leaky_relu(self.bn1(self.conv1(x))))
+        x = self.pool(self.leaky_relu(self.bn2(self.conv2(x))))
+        x = self.pool(self.leaky_relu(self.bn3(self.conv3(x))))
+        x = self.pool(self.leaky_relu(self.bn4(self.conv4(x))))
+        x = self.pool(self.leaky_relu(self.bn5(self.conv5(x))))
+        x = self.dropout(x)
+        x = x.view(-1, 512 * 7 * 7)
+        x = self.leaky_relu(self.fc1(x))
+        x = self.dropout(x)
+        x = self.leaky_relu(self.fc2(x))
+        x = self.fc3(x)
+        return x
+
+class EmotionResNet(nn.Module):
+    def __init__(self, num_classes=7):
+        super(EmotionResNet, self).__init__()
+        self.resnet = models.resnet18(pretrained=True)
+        self.resnet.conv1 = nn.Conv2d(1, 64, kernel_size=7, stride=2, padding=3, bias=False)
+        self.resnet.fc = nn.Linear(self.resnet.fc.in_features, num_classes)
+
+    def forward(self, x):
+        return self.resnet(x)
