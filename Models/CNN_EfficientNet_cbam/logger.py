e525c79e0983ef8db708afa20e7cd21b6947573e
Abdulmalik <abdulmalikshinkafi@gmail.com>
2024-09-29 12:15:43 +0100

    Included Efficientnet with CBAM

---
+++ Models/CNN_EfficientNet_cbam/logger.py
@@ -0,0 +1,9 @@
+import wandb
+
+class Logger:
+    def __init__(self, experiment_name, project='FER_Thesis', logger_name='logger', dir=None):
+        logger_name = f'{logger_name}-{experiment_name}'
+        self.logger = wandb.init(project=project, name=logger_name, dir=dir, reinit=False)
+    
+    def get_logger(self):
+        return self.logger
